2cc16798646f4dd2933a0f11e8f131a569d699bf
MagoroGraphics <magoro.graphics@gmail.com>
2023-01-18 12:59:23 +0000

    Initial commit

---
+++ src/main/java/covatars/com/Covatars/CovatarsApplication.java
@@ -0,0 +1,13 @@
+package covatars.com.Covatars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovatarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovatarsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/covatars/com/Covatars/CovatarsApplicationTests.java
@@ -0,0 +1,13 @@
+package covatars.com.Covatars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovatarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
