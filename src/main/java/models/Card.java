e74c3b8adfd4dd12c5185905602b6ad3beecc099
MagoroGraphics <magoro.graphics@gmail.com>
2023-01-19 16:19:19 +0000

    Creates Card model

--- src/main/java/models/Card.java
+++ src/main/java/models/Card.java
@@ -1,4 +1,66 @@
 package models;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "cards")
 public class Card {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable=false)
+    private Long id;
+
+    @Column
+    private String type;
+
+    @Column
+    private String color;
+
+    @Column
+    private String imageUrl;
+
+    public Card() {
+    }
+
+    public Card(String type, String color, String imageUrl) {
+        this.type = type;
+        this.color = color;
+        this.imageUrl = imageUrl;
+    }
+
+    //region Getters and Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    //endregion
 }
