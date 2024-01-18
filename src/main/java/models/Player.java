bb01007f289f11e89972486a557253f699deb47c
MagoroGraphics <magoro.graphics@gmail.com>
2024-01-18 16:44:38 +0000

    Add more properties to player

--- src/main/java/models/Player.java
+++ src/main/java/models/Player.java
@@ -2,6 +2,8 @@ package models;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+
 @Entity
 @Table(name="players")
 public class Player {
@@ -13,5 +15,51 @@ public class Player {
 @Column
     private String name;
 
+@Column
+    private Integer score;
 
+@Column
+    private ArrayList<Card> cards;
+
+public Player(){
+
+}
+
+    public Player(String name, Integer score, ArrayList<Card> cards) {
+        this.name = name;
+        this.score = score;
+        this.cards = cards;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getScore() {
+        return score;
+    }
+
+    public void setScore(Integer score) {
+        this.score = score;
+    }
+
+    public ArrayList<Card> getCards() {
+        return cards;
+    }
+
+    public void setCards(ArrayList<Card> cards) {
+        this.cards = cards;
+    }
 }
