6d55facb657351983cf504cb78674a490a021af3
MagoroGraphics <magoro.graphics@gmail.com>
2024-02-15 20:59:01 +0000

    Adds Virus1 card class

--- src/main/java/covatars/com/Covatars/controllers/CardController.java
+++ src/main/java/covatars/com/Covatars/controllers/CardController.java
@@ -19,3 +19,4 @@ public class CardController {
         return cardRepository.findAll();
     }
 }
+
---
+++ src/main/java/covatars/com/Covatars/models/Virus1.java
@@ -0,0 +1,11 @@
+package covatars.com.Covatars.models;
+
+public class Virus1 extends Card{
+
+    public Virus1(String type, String color, String imageUrl, Integer value) {
+        super(type, color, imageUrl, value);
+        setType("virus1");
+        setValue(-1);
+    }
+
+}
